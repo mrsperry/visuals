e05fe8bdfa7ff43c8f9c3e83737c5978c795c882
Josh Sperry <mrjoshuasperry@gmail.com>
2020-01-11 05:01:59 -0500

    Add a random constellation simulator

---
+++ geometric-constellations/js/main.js
@@ -0,0 +1,80 @@
+function setup() {
+    main.setup();
+}
+
+function draw() {
+    main.draw();
+}
+
+class main {
+    // The maximum number of stars allowed on screen at once
+    static maxStars = 100;
+    // The cooldown timer to add new stars, preventing stalls in new spawns
+    static spawnCooldown = 5;
+
+    // The current stars
+    static stars = [];
+
+    static setup() {
+        createCanvas(window.innerWidth, window.innerHeight);
+    }
+
+    static draw() {
+        // Clear the screen
+        background(0);
+
+        // Check if the cooldown has expired
+        if (frameCount % main.spawnCooldown == 0) {
+            // Check if the max number of stars has been reached
+            if (main.stars.length < main.maxStars) {
+                // Add a new star
+                main.stars.push(new star());
+            }
+        }
+
+        // Create a list of star connections
+        const connections = [];
+        main.stars.map(item => {
+            // Reset this star's connections
+            item.connections = 0;
+
+            for (const current of main.stars) {
+                // Skip over this star as it can't connect to itself
+                if (current == item) {
+                    continue;
+                }
+
+                // Get the distance between this star and the comparing star
+                const distance = dist(item.position.x, item.position.y, current.position.x, current.position.y);
+                // Check if stars can connect
+                if (distance <= star.connectorRadius) {
+                    // Add this connection
+                    connections.push({
+                        star1: item,
+                        star2: current,
+                        // Calculate the color based on the distance
+                        color: lerpColor(color(0), color(255), (star.connectorRadius - distance) / star.connectorRadius)
+                    })
+
+                    // Increase this star's connections
+                    item.connections += 1;
+                }
+            }
+        });
+
+        // Sort connections by color value, lower being drawn first (only check red level since connectors are grayscale)
+        connections.sort((star1, star2) => star1.color.levels[0] - star2.color.levels[0]);
+        // Draw all connections (must happen before stars are drawn or they will overlap)
+        connections.map(data => {
+            stroke(data.color);
+            line(data.star1.position.x, data.star1.position.y, data.star2.position.x, data.star2.position.y);
+        });
+
+        noStroke();
+        // Draw all stars
+        main.stars = main.stars.filter(star => {
+            star.draw();
+            return star.progress <= 1;
+        });
+    }
+}
---
+++ geometric-constellations/js/star.js
@@ -0,0 +1,68 @@
+class star {
+    // How far a star can connect to another star in pixels
+    static connectorRadius = 100;
+    // The minimum size of a star
+    static minSize = 4;
+    // The maximum size of a star
+    static maxSize = 10;
+    // The minimum grayscale value of a star
+    static minColor = 130;
+    // The maximum grayscale value of a star
+    static maxColor = 200;
+    // The minimum speed of a star, measured in frames
+    static minSpeed = 0.0001;
+    // The maximum speed of a star measured in frames
+    static maxSpeed = 0.0005;
+
+    size = round(random(star.minSize, star.maxSize));
+    color = round(random(star.minColor, star.maxColor));
+    speed = random(star.minSpeed, star.maxSpeed);
+    // The progress percent (0-1) of this star's path
+    progress = 0;
+    // The number of stars conencted to this star
+    connections = 0;
+
+    constructor() {
+        const getPosition = () => {
+            // Get the axis this star will get a random coordinate for
+            const axis = round(random()) == 0;
+            // If the opposite axis should have its value revered
+            const reverse = round(random()) == 0;
+            // Amount of offset needed to hide the spawning of the star
+            const offset = (this.size * 2) + 10;
+
+            // Get a random coordinate for each axis
+            const x = round(random(window.innerWidth));
+            const y = round(random(window.innerHeight));
+
+            // Set the coordinates to spawn on (either top-bottom or left-right)
+            return {
+                x: axis ? (reverse ? -offset : window.innerWidth + offset) : x,
+                y: axis ? y : (reverse ? -offset : window.innerHeight + offset)
+            };
+        };
+
+        // Set the initial position of the star
+        this.startPosition = getPosition();
+        // Set the end position of the star
+        this.endPosition = getPosition();
+        // Set the current position of the star
+        this.position = {
+            x: this.startPosition.x,
+            y: this.startPosition.y
+        };
+    }
+
+    draw() {
+        this.progress += this.speed;
+
+        // Set the new position of this star
+        this.position.x = lerp(this.startPosition.x, this.endPosition.x, this.progress);
+        this.position.y = lerp(this.startPosition.y, this.endPosition.y, this.progress);
+
+        // Set the color of this star
+        fill(this.color + (this.connections * 5));
+        // Draw the star
+        circle(this.position.x, this.position.y, this.size);
+    }
+}
