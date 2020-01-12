ba82d0b89b0681909ed710e23a688d6fff40aa1f
Josh Sperry <mrjoshuasperry@gmail.com>
2020-01-12 02:37:00 -0500

    Add files for github pages

---
+++ src/js/Main.js
@@ -0,0 +1,54 @@
+class Main {
+    // List of all visual project names
+    static projects = [
+        "falling-lights",
+        "geometric-constellations",
+        "rainbow-black-hole"
+    ];
+
+    static initialize() {
+        for (const name of Main.projects) {
+            // Create the image container
+            const container = $("<div>")
+                .addClass("project")
+                .appendTo($("#project-list"));
+
+            // Make sure the entire container can be clicked on
+            const link = $("<a>")
+                .addClass("flex flex-justify-center flex-align-center")
+                .attr("href", "projects/" + name)
+                .appendTo(container);
+
+            // Set the image
+            $("<img>")
+                .attr("id", name)
+                .attr("src", "src/images/" + name + ".png")
+                .appendTo(link);
+
+            // Set the title
+            const title = $("<div>")
+                .addClass("title bold centered")
+                .text(Main.idToText(name))
+                .hide()
+                .appendTo(link);
+
+            // Have the title fade in/out on hover
+            container.hover(() => {
+                title.stop().fadeIn(300);
+            }, () => {
+                title.stop().fadeOut(300);
+            });
+        }
+    }
+
+    static idToText(id) {
+        let result = "";
+        for (const word of id.split("-")) {
+            // Capatalize the word and add a space
+            result += word[0].toUpperCase() + word.substring(1, word.length) + " ";
+        }
+
+        // Remove trailing space
+        return result.substring(0, result.length - 1);
+    }
+}
